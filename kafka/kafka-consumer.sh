0d49702adccc861d9c7b782a2e2ecaab15a690b9
Masatoshi Itoh <masatoshi9953@gmail.com>
2023-05-16 15:40:51 +0000

    あとはkafkaのadditional listenerをサービスとしてexposeするだけのはず...

---
+++ kafka/kafka-consumer.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# kubectl -n kafka run kafka-consumer -ti --image=quay.io/strimzi/kafka:0.34.0-kafka-3.4.0 --rm=true --restart=Never -- bin/kafka-console-consumer.sh --bootstrap-server my-cluster-kafka-bootstrap:9092 --topic my-topic --from-beginning
+kubectl run kafka-consumer -ti --image=quay.io/strimzi/kafka:0.34.0-kafka-3.4.0 --rm=true --restart=Never -- bin/kafka-console-consumer.sh --bootstrap-server kafka:9092 --topic my-topic --from-beginning
---
+++ kafka/kafka-producer.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# kubectl -n kafka run kafka-producer -ti --image=quay.io/strimzi/kafka:0.34.0-kafka-3.4.0 --rm=true --restart=Never -- bin/kafka-console-producer.sh --bootstrap-server my-cluster-kafka-bootstrap:9092 --topic my-topic
+kubectl run kafka-producer -ti --image=quay.io/strimzi/kafka:0.34.0-kafka-3.4.0 --rm=true --restart=Never -- bin/kafka-console-producer.sh --bootstrap-server kafka:9092 --topic my-topic
